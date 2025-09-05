8acb495c7d59774ebbd954b881841da17fceb255
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-09-05 12:51:22 -0300

    ProductControllerRA findAll tests Ok

--- src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
+++ src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
@@ -10,11 +10,13 @@ import org.junit.jupiter.api.Test;
 public class ProductControllerRA {
 	
 	private Long existingId, nonExistingId;
+	private String productName;
 	
 	@BeforeEach
 	private void setUp() {
 		baseURI = "http://localhost:8080"; //endereço da api que eu quero testar os endpoints, seria o projeto dscommerce-jacoco-tests
 
+		productName = "Macbook";
 	}
 	
 	@Test
@@ -31,6 +33,39 @@ public class ProductControllerRA {
 				.body("price", is(2190.0F))
 					.body("categories.id", hasItems(2, 3))
 					.body("categories.name", hasItems("Eletrônicos", "Computadores"));
+	}
+	
+	@Test
+	public void findAllShouldReturnPageProductsWhenProductNameIsEmpty() {
+		
+		given()
+			.get("/products")
+			.then()
+				.statusCode(200)
+				.body("content.name", hasItems("Macbook Pro", "PC Gamer Tera"));
+	}
+	
+	@Test
+	public void findAllShouldReturnPageProductsWhenProductNameIsNotEmpty() {
+		
+		given()
+			.get("/products?name={productName}", productName)
+			.then()
+				.statusCode(200)
+				.body("content.id[0]", is(3))
+				.body("content.name[0]", equalTo("Macbook Pro"))
+				.body("content.price[0]", is(1250.0F))
+				.body("content.imgUrl[0]", equalTo("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"));
+	}
+	
+	@Test
+	public void findAllShouldReturnPageProductsWhenProductPriceIsGreaterThen2000() {
+		
+		given()
+			.get("/products")
+			.then()
+				.statusCode(200)
+				.body("content.findAll { it.price > 2000 }.name ", hasItems("Smart TV", "PC Gamer Weed"));
 	}
 
 }
