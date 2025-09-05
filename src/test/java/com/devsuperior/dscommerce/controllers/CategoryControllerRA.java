ab3f3aa181360d1a79f63f48867ebd763cd8f9a1
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-09-05 19:26:27 -0300

    CategoryControllerRA findAll test Ok

---
+++ src/test/java/com/devsuperior/dscommerce/controllers/CategoryControllerRA.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dscommerce.controllers;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CategoryControllerRA {
+	
+	@BeforeEach
+	public void setup() {
+		baseURI = "http://localhost:8080";
+	}
+	
+	@Test
+	public void findAllShouldReturnListOfCategories() {
+		given()
+			.get("/categories")
+		.then()
+			.statusCode(200)
+			.body("id", hasItems(1, 2, 3))
+			.body("name", hasItems("Livros", "Eletrônicos", "Computadores"));
+	}
+
+}
